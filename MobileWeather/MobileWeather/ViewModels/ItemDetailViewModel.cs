37299f832d8b77819550be74ec203a6b0b23aa22
speyck <54800864+speyck@users.noreply.github.com>
2021-12-07 15:08:34 +0100

    Add default xamarin.forms project with solution

---
+++ MobileWeather/MobileWeather/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MobileWeather.Models;
+
+namespace MobileWeather.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
