0be20fd2f6f1c8779db1914f08e4ab32b94cba7a
speyck <54800864+speyck@users.noreply.github.com>
2021-12-14 12:26:15 +0100

    Get Location from GPS on gps-button click

--- MobileWeather/MobileWeather/Pages/WeatherPage.xaml.cs
+++ MobileWeather/MobileWeather/Pages/WeatherPage.xaml.cs
@@ -4,6 +4,13 @@ using System;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+using ModelLocation = MobileWeather.Models.Location;
+using XamarinLocation = Xamarin.Essentials.Location;
+using System.Collections.Generic;
+using System.Linq;
+using RestSharp;
 
 namespace MobileWeather.Pages
 {
@@ -24,14 +31,30 @@ namespace MobileWeather.Pages
             Navigation.PushAsync(new SearchPage());
         }
 
-        private void GPS_Clicked(object sender, EventArgs e)
+        private async void GPS_Clicked(object sender, EventArgs e)
         {
+            XamarinLocation geo = await Geolocation.GetLastKnownLocationAsync();
 
+            IEnumerable<Placemark> placemarks = await Geocoding.GetPlacemarksAsync(Math.Round(geo.Latitude, 3), Math.Round(geo.Longitude, 3));
+
+            Placemark mark = placemarks?.FirstOrDefault();
+
+            ModelLocation location = new ModelLocation()
+            {
+                Address = mark.Thoroughfare,
+                Area = mark.SubLocality,
+                City = mark.Locality,
+                Zip = mark.PostalCode,
+                State = mark.AdminArea,
+                Country = mark.CountryName
+            };
+
+            _service.CurrentLocation = location;
         }
 
         private void OnLocationChanged_Handler(object sender, LocationChangedEventArgs e)
         {
-            lblLocation.Text = e.Location.Name;
+            lblLocation.Text = e.Location.City;
         }
     }
 }
