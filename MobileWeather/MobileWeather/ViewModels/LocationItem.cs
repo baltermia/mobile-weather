9076f0a398a568d83ffb84716cbfbef0722118a1
speyck <54800864+speyck@users.noreply.github.com>
2021-12-14 12:22:13 +0100

    Add LocationObject in LocationItem ViewModel

--- MobileWeather/MobileWeather/ViewModels/LocationItem.cs
+++ MobileWeather/MobileWeather/ViewModels/LocationItem.cs
@@ -1,7 +1,11 @@
-﻿namespace MobileWeather.ViewModels
+﻿using MobileWeather.Models;
+
+namespace MobileWeather.ViewModels
 {
     public class LocationItem
     {
+        public Location LocationObject { get; set; }
+
         public string Location { get; set; }
         public string Country { get; set; }
         public string County { get; set; }
