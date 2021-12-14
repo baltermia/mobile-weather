07ca3b4ce53026804e749fb8bdc534fa0e742e21
speyck <54800864+speyck@users.noreply.github.com>
2021-12-14 12:25:14 +0100

    Add Location properties to Location class

--- MobileWeather/MobileWeather/Models/Location.cs
+++ MobileWeather/MobileWeather/Models/Location.cs
@@ -2,6 +2,11 @@
 {
     public class Location
     {
-        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Area { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
     }
 }
