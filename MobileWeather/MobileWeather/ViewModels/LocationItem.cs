f1f77782ab1a57815ef2893fb12fd7dcbca5e07d
speyck <54800864+speyck@users.noreply.github.com>
2021-12-14 10:34:22 +0100

    Add LocationItem ViewModel for SearchPage ListView

---
+++ MobileWeather/MobileWeather/ViewModels/LocationItem.cs
@@ -0,0 +1,16 @@
+﻿namespace MobileWeather.ViewModels
+{
+    public class LocationItem
+    {
+        public string Location { get; set; }
+        public string Country { get; set; }
+        public string County { get; set; }
+
+        public string CountryCountyString => GetCountryCountyString();
+
+        public string GetCountryCountyString()
+        {
+            return Country + ", " + County;
+        }
+    }
+}
