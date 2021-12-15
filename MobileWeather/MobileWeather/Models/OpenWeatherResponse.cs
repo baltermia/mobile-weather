d980ea87a5d5a47d84a2604d7765d920f52be0b6
speyck <54800864+speyck@users.noreply.github.com>
2021-12-15 10:34:29 +0100

    Add OpenWeatherResponse model class

---
+++ MobileWeather/MobileWeather/Models/OpenWeatherResponse.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace MobileWeather.Models
+{
+    public class OpenWeatherResponse
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("weather")]
+        public IEnumerable<Weather> WeatherList { get; set; }
+
+        [JsonProperty("main")]
+        public Temperature Temperature { get; set; }
+    }
+
+    public class Weather
+    {
+        [JsonProperty("main")]
+        public string Description { get; set; }
+
+        [JsonProperty("icon")]
+        public string IconId { get; set; }
+    }
+
+    public class Temperature
+    {
+        [JsonProperty("temp")]
+        public double Main { get; set; }
+    }
+}
