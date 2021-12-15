b5035dfc5548ec4edbe293d0ed2b7c765f600b05
speyck <54800864+speyck@users.noreply.github.com>
2021-12-15 09:44:53 +0100

    Add GooglePlacesAPI for location reccomendations

--- MobileWeather/MobileWeather/Pages/SearchPage.xaml.cs
+++ MobileWeather/MobileWeather/Pages/SearchPage.xaml.cs
@@ -4,6 +4,12 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using System.Collections.ObjectModel;
 using MobileWeather.ViewModels;
+using RestSharp;
+using Newtonsoft.Json;
+using System.Linq;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System;
 
 namespace MobileWeather.Pages
 {
@@ -12,7 +18,8 @@ namespace MobileWeather.Pages
     {
         private readonly LocationService _service;
 
-        private readonly ObservableCollection<LocationItem> Locations = new ObservableCollection<LocationItem>();
+        private readonly RestClient client = new RestClient("https://maps.googleapis.com/maps/api/place/autocomplete/json");
+        private readonly string apiKey = Data.Secrets.GooglePlacesAPI;
 
         public SearchPage()
         {
@@ -23,14 +30,41 @@ namespace MobileWeather.Pages
 
         private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
         {
+            RestRequest request = GetDefaultRequest();
+            request.AddParameter("input", sbrSearch.Text);
 
+            IRestResponse response = client.Execute(request);
+
+            AutocompleteResultJson jsonResult = JsonConvert.DeserializeObject<AutocompleteResultJson>(response.Content);
+
+            lstResults.ItemsSource = new ObservableCollection<LocationItem>(jsonResult.AutocompleteResults.Select(r => new LocationItem()
+            {
+                Location = r.Details.Main,
+                Details = r.Details.Secondary
+            }));
         }
 
-        private void Locations_ItemTapped(object sender, ItemTappedEventArgs e)
+        private async void Locations_ItemTapped(object sender, ItemTappedEventArgs e)
         {
             LocationItem location = e.Item as LocationItem;
 
-            _service.CurrentLocation = location.LocationObject;
+            _service.CurrentLocation = new Location()
+            {
+                City = location.Location
+            };
+
+            await Navigation.PopAsync();
+        }
+
+        private RestRequest GetDefaultRequest(Method method = Method.GET)
+        {
+            RestRequest request = new RestRequest(method);
+
+            request.AddParameter("types", "(cities)");
+            request.AddParameter("key", apiKey);
+
+            return request;
+
         }
     }
 }
