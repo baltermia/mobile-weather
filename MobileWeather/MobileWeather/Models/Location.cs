f44f621fb607372e98c74aae5a53ea7c2ecf39f2
speyck <54800864+speyck@users.noreply.github.com>
2021-12-15 09:40:24 +0100

    Remove unnecessary properties from Location model

--- MobileWeather/MobileWeather/Models/Location.cs
+++ MobileWeather/MobileWeather/Models/Location.cs
@@ -2,12 +2,6 @@
 {
     public class Location
     {
-        public string Address { get; set; }
-        public string Area { get; set; }
         public string City { get; set; }
-        public string Zip { get; set; }
-        public string State { get; set; }
-        public string Country { get; set; }
-        public string CountryCode { get; set; }
     }
 }
