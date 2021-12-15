dde73f3a9298c1d6add1e51a3365864f66343399
speyck <54800864+speyck@users.noreply.github.com>
2021-12-15 09:39:47 +0100

    Remove unnessecary properties from LocationItem and add Details property

--- MobileWeather/MobileWeather/ViewModels/LocationItem.cs
+++ MobileWeather/MobileWeather/ViewModels/LocationItem.cs
@@ -4,17 +4,7 @@ namespace MobileWeather.ViewModels
 {
     public class LocationItem
     {
-        public Location LocationObject { get; set; }
-
         public string Location { get; set; }
-        public string Country { get; set; }
-        public string County { get; set; }
-
-        public string CountryCountyString => GetCountryCountyString();
-
-        public string GetCountryCountyString()
-        {
-            return Country + ", " + County;
-        }
+        public string Details { get; set; }
     }
 }
