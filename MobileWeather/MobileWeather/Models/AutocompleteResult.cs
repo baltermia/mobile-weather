477e9665f4ed59851dfd195c1ff9968cd76f35d5
speyck <54800864+speyck@users.noreply.github.com>
2021-12-15 09:07:09 +0100

    Add AutocompleteResult(Json) model classes

---
+++ MobileWeather/MobileWeather/Models/AutocompleteResult.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace MobileWeather.Models
+{
+    public class AutocompleteResult
+    {
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("structured_formatting")]
+        public AutocompleteResultDetails Details { get; set; }
+
+        public class AutocompleteResultDetails
+        {
+            [JsonProperty("main_text")]
+            public string Main { get; set; }
+
+            [JsonProperty("secondary_text")]
+            public string Secondary { get; set; }
+        }
+    }
+}
---
+++ MobileWeather/MobileWeather/Models/AutocompleteResultJson.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace MobileWeather.Models
+{
+    public class AutocompleteResultJson
+    {
+        [JsonProperty("predictions")]
+        public IEnumerable<AutocompleteResult> AutocompleteResults { get; set; }
+    }
+}
